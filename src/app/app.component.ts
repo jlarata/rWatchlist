a35cdefe0fd35b104ef33a3a638d6075a4782e21
jlarata <ariel@cfee.gob.ar>
2024-08-22 13:45:53 -0300

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WatchListComponent } from "./watch-list/watch-list.component";
 import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WatchListComponent, HeaderComponent],
+  imports: [RouterOutlet, WatchListComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
